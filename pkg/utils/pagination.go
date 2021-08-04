5f5712fde4dd902f29f23bdfca90afd362d7c4aa
Ali Kaviani <a.kaviani@sabaidea.com>
2021-08-04 21:24:24 +0430

    Feat: adding more-pagination structure

--- pkg/utils/pagination.go
+++ pkg/utils/pagination.go
@@ -4,3 +4,8 @@ type Paging struct {
 	Page int `validate:"required,min=1"`
 	Size int `validate:"required,min=5,max=100"`
 }
+
+type MorePaging struct {
+	LastID uint `validate:"required,min=0"`
+	Size   int `validate:"required,min=5,max=300"`
+}
